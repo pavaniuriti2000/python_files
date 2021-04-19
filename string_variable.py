d5a122cfd95e43e79b7ec377f872b99f67775065
unknown <pavani.uriti123@gmail.com>
2021-04-19 13:39:19 +0530

    String variable practice

---
+++ string_variable.py
@@ -0,0 +1,10 @@
+#String variables practice
+
+# me and My friend shyamala attended the workshop conducted by APSSDC on "Android App Dvelopment" and we faced a lot of problems with android studio installation and all.
+me ="Me"
+
+friend=input("Enter your friend name: ")
+workshop="APSSDC"
+workshop_on="""Android App Development"""
+problem_faced="ANdroid studio instalation"
+print(me+" "+"and My friend "+friend+" "+"attnded the workshop conducted by "+workshop+" and we faced a lot of problems with "+problem_faced+" "+"and all")
